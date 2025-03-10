66490bacdf90e0cc5e9e085a4bf0a2f779bdce63
sindre-gangeskar <sindre.gangeskar2710@outlook.com>
2025-03-10 18:16:40 +0100

    Shortened secure propertry for session cookie

--- front-end/app.js
+++ front-end/app.js
@@ -56,7 +56,7 @@ app.use(session({
     maxAge: 1000 * 60 * 60 * 2,
     httpOnly: true,
     sameSite: 'lax',
-    secure: process.env.NODE_ENV === 'production' ? true : false
+    secure: process.env.NODE_ENV === 'production'
   },
   rolling: true
 }))
