1ee30ba1f888f772556eec749f3f2451520297ed
Sindre Gangeskar <sindregangeskar@outlook.com>
2026-04-01 14:52:56 +0200

    Remove unused npm package and add ratelimit cleanup

--- back-end/middleware/ratelimit.js
+++ back-end/middleware/ratelimit.js
@@ -4,11 +4,6 @@ const { createAndThrowError } = require('../javascripts/utils');
 module.exports = async (req, res, next) => {
   try {
     const key = `ip:${req.ip}`;
-    const xForwardedFor = req.headers[ "x-forwarded-for" ];
-
-    console.log('Rate limit key:', key);
-    console.log('x-forwarded-for header:', xForwardedFor);
-    
     const { success, pending } = await rateLimit.limit(key, {
       ip: req.ip,
       userAgent: req.get('user-agent') || "unknown",
